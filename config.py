66fa4ade812e97debf7546857e8ffefa0fe3dd44
Muzna <muzna.safialdin@gmail.com>
2025-04-16 08:47:00 +0300

    updated delete

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+#SECRET_KEY = 'hjdsbjdakfjkakfjsfbkhjbfknjgzbziuhdhsdjdnjff'
+
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
