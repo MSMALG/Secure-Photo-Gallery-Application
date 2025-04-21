f726169c282259104a5000e7b14c6f37d19f81df
Muzna <91617542+MSMALG@users.noreply.github.com>
2025-04-21 20:04:44 +0300

    Update image_model.py

--- models/image_model.py
+++ models/image_model.py
@@ -2,12 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 from models import db
 
 
-
 class File(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     filename = db.Column(db.String(200), nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
     user = db.relationship('User', backref='files') 
     is_encrypted = db.Column(db.Boolean, default=False)   #encryption status  
-
-
